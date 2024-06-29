36727d28c72579a9bc1111bce5d08367650bbb14
dream-build2020 <kindtom123@gmail.com>
2024-06-29 14:29:00 +0800

    更新celery

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,9 +1,14 @@
 import logging
-from core.CeleryCore import celery_app
+from celery import Celery
 # from utils.config import settings
 
 
 LOGGER = logging.getLogger(__name__)
+
+celery_app = Celery('tasks',
+                    broker='redis://localhost:6379/0',
+                    include=["tasks.tasks"]
+                    )
 
 
 class Tasks(object):
