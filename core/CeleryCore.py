d68959479f465f53de967c7396e1ec5257640407
dream-build2020 <kindtom123@gmail.com>
2024-07-07 21:47:04 +0800

    更新celery

--- core/CeleryCore.py
+++
@@ -1,8 +0,0 @@
-from celery import Celery
-# from utils.config import settings
-
-
-celery_app = Celery('tasks',
-                    broker='redis://localhost:6379/0',
-                    include=["tasks.tasks"]
-                    )
